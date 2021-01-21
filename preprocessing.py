d95e220e4580fa553a8afa6ddaf2e337d8a7627e
ztmajor <mylovezju@163.com>
2021-01-21 11:03:04 +0800

    2

--- preprocessing.py
+++ preprocessing.py
@@ -13,12 +13,21 @@ def merge_data(data_path, name_list):
         pd.read_csv(os.path.join(data_path, "weather_" + name + ".csv"))
 
 
+def set_year(data_name):
+    data = pd.read_csv("Dataset/place/weather_{}.csv".format(data_name))
+    data["year"] = data["year"].apply(lambda x: 2020)
+    data.to_csv("Dataset/weather_{}.csv".format(data_name), index=False)
+
 
 if __name__ == "__main__":
-    for raw_data_file in raw_data_file_list:
-        print("\n------------ 1 load {} ------------".format(raw_data_file))
-        weather_data = pd.read_csv(raw_data_file)
-        print(weather_data.head())
+    # for raw_data_file in raw_data_file_list:
+    #     print("\n------------ 1 load {} ------------".format(raw_data_file))
+    #     weather_data = pd.read_csv(raw_data_file)
+    #     print(weather_data.head())
+    #
+    #     print("\n------------ 2 split data into train set and valid set ------------")
+    #     split_data_set(weather_data, weather_data.columns, valid_num=1*12)
 
-        print("\n------------ 2 split data into train set and valid set ------------")
-        split_data_set(weather_data, weather_data.columns, valid_num=1*12)
+    name_list = ["city_museum", "spin_museum", "memorial", "yuhua", "niushou", "confucius", "zhongshan", "president", "ginkgo", "zoo"]
+    for name in name_list:
+        set_year(name)
